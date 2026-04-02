e7cc7825ca2b3f7aee921ea472311cbf4e719fe4
Feride <feride.kaya642@gmail.com>
2026-04-02 16:45:00 +0300

    feat(assignments): register assignment models in Django admin

--- assignments/admin.py
+++ assignments/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Assignment, TaskSubmission
 
-# Register your models here.
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'task', 'shift', 'date', 'status', 'assigned_by')
+    list_filter = ('status', 'date', 'shift')
+    search_fields = ('user__name', 'task__title')
+
+
+@admin.register(TaskSubmission)
+class TaskSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('assignment', 'approval_status', 'submitted_at', 'approved_by')
+    list_filter = ('approval_status',)
