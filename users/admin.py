aee0da524a761d2c0431bd91c5fceedf98085739
Feride <feride.kaya642@gmail.com>
2026-04-02 16:41:20 +0300

    feat(users): register User model in Django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'name', 'role', 'gender', 'is_active', 'created_at')
+    list_filter = ('role', 'gender', 'is_active')
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal', {'fields': ('name', 'gender')}),
+        ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'role'),
+        }),
+    )
