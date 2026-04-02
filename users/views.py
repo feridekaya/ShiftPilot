fa4225b35762d27f85e3840e7c5a56b24fa65dd6
Feride <feride.kaya642@gmail.com>
2026-04-02 16:38:30 +0300

    feat(auth): add register and me views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from .models import User
+from .permissions import IsManager
+from .serializers import (
+    CustomTokenObtainPairSerializer,
+    RegisterSerializer,
+    UserSerializer,
+    UserCreateSerializer,
+    UserUpdateSerializer,
+)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
+    def perform_create(self, serializer):
+        # Unauthenticated callers always get role='employee'
+        if not self.request.user or not self.request.user.is_authenticated:
+            serializer.save(role='employee')
+        else:
+            serializer.save()
+
+
+class MeView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsManager]
+    queryset = User.objects.all().order_by('name')
+    http_method_names = ['get', 'post', 'put', 'delete', 'head', 'options']
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserCreateSerializer
+        if self.action in ('update', 'partial_update'):
+            return UserUpdateSerializer
+        return UserSerializer
