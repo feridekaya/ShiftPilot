eba4d739b8907483e2f85052af6f664b61103876
Feride <feride.kaya642@gmail.com>
2026-04-02 16:42:52 +0300

    feat(tasks): register task models in Django admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Zone, Shift, Task, TaskSchedule
 
-# Register your models here.
+
+@admin.register(Zone)
+class ZoneAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+
+@admin.register(Shift)
+class ShiftAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_time', 'end_time')
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'zone', 'coefficient', 'requires_photo', 'created_by')
+    list_filter = ('zone', 'requires_photo')
+    search_fields = ('title',)
+
+
+@admin.register(TaskSchedule)
+class TaskScheduleAdmin(admin.ModelAdmin):
+    list_display = ('task', 'frequency')
+    list_filter = ('frequency',)
